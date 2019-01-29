b85eb4c5a75f8b1ede29249e0b75d2afbe729869
Leixiujie <982463478@qq.com>
2019-01-29 21:43:15 +0800

    这两个文件可以用更小内存train模型
    这两个文件可以用更小内存train模型，但是运行的时候先要运行dataset_less_memory.py  来创建分段样本对

---
+++ dataset_less_memory.py
@@ -0,0 +1,159 @@
+import numpy as np
+from PIL import Image
+from config import FLAGS
+import os
+
+DEV_NUMBER = FLAGS.DEV_NUMBER
+BASE_PATH = FLAGS.BASE_PATH
+batch_size = FLAGS.batch_size
+
+        
+
+
+print('mark: dividing the negative dataset')
+
+def dataset_divide():
+    negative_pairs_path_file = open(FLAGS.negative_file, 'r')
+    negative_pairs_path_lines = negative_pairs_path_file.readlines()
+    negative_pairs_path_file.close()
+    positive_pairs_path_file = open(FLAGS.positive_file, 'r')
+    positive_pairs_path_lines = positive_pairs_path_file.readlines()
+    positive_pairs_path_file.close()
+    print(len(negative_pairs_path_lines))
+    print(len(positive_pairs_path_lines))
+    
+    left_image_path_list = []
+    right_image_path_list = []
+    similar_list = []
+    for line in negative_pairs_path_lines:
+        left_right = line.strip().split(' ')
+        left_image_path_list.append(left_right[0])
+        right_image_path_list.append(left_right[1])
+        similar_list.append(0)
+    negative_pairs_path_lines = []
+    
+    
+    for line in positive_pairs_path_lines:
+        left_right = line.strip().split(' ')
+        left_image_path_list.append(left_right[0])
+        right_image_path_list.append(left_right[1])
+        similar_list.append(1)  
+    positive_pairs_path_lines = []
+    
+    print('mark: added positve and negative array ')
+    left_image_path_list = np.asarray(left_image_path_list)
+    right_image_path_list = np.asarray(right_image_path_list)
+    similar_list = np.asarray(similar_list)
+    
+    np.random.seed(10)
+    shuffle_indices = np.random.permutation(np.arange(len(similar_list)))
+    left_shuffled = left_image_path_list[shuffle_indices]
+    left_image_path_list = []
+    right_shuffled = right_image_path_list[shuffle_indices]
+    right_image_path_list = []
+    similar_shuffled = similar_list[shuffle_indices]
+    similar_list = []
+    print('mark: shuffle completed')
+    
+    file_num = 0
+    file_name = 'pairs_'+str(file_num).strip() +'.txt'
+    pairs_shuffled_path = './datas/pairs/'
+    if not os.path.exists(pairs_shuffled_path):
+        os.makedirs(pairs_shuffled_path)
+    file_path = os.path.join(pairs_shuffled_path,file_name)
+    file_num += 1
+    f = open(file_path,'w')
+    for i in range(len(similar_shuffled)):
+        if ((i % 2000000 == 0 ) and i != 0):
+            f.close()
+            file_name = 'pairs_'+str(file_num).strip()+'.txt'
+            file_path = os.path.join(pairs_shuffled_path,file_name)
+            f = open(file_path,'w')
+            file_num += 1
+        f.write(str(left_shuffled[i]).strip() + ' ' + str(right_shuffled[i]).strip() + ' ' + str(similar_shuffled[i]).strip() + '\n')
+        
+        if(i % 1000000 == 0):
+            print(i)
+            
+        if(i == (len(similar_shuffled)-1)):
+            f.close()
+
+        
+    
+
+def get_dataset(pairs_file,devset_need_or_not = False):
+    f = open(pairs_file,'r')
+    pairs_path_lines = f.readlines()
+    print('txt 长度是 '+str(len(pairs_path_lines)))
+    f.close()
+    
+    
+    left_image_path_list = []
+    right_image_path_list = []
+    similar_list = []
+    for line in pairs_path_lines:
+        left_right_similar = line.strip().split(' ')
+        left_image_path_list.append(left_right_similar[0])
+        right_image_path_list.append(left_right_similar[1])
+        similar_list.append(eval(left_right_similar[2]))
+    pairs_path_lines = []
+    
+    print('mark: added positve and negative array ')
+    left_image_path_list = np.asarray(left_image_path_list)
+    right_image_path_list = np.asarray(right_image_path_list)
+    similar_list = np.asarray(similar_list)
+    
+    if devset_need_or_not :
+        left_train = left_image_path_list[:DEV_NUMBER]
+        left_dev = left_image_path_list[DEV_NUMBER:]
+        left_image_path_list = []
+        right_train = right_image_path_list[:DEV_NUMBER]
+        right_dev = right_image_path_list[DEV_NUMBER:]
+        right_image_path_list = []
+        similar_train, similar_dev = similar_list[:DEV_NUMBER], similar_list[DEV_NUMBER:]
+        similar_list = []
+        print('mark: shuffle completed')
+        print(len(left_train))
+        print(len(right_train))
+        print(len(left_dev))
+        print(len(right_dev))
+        print(len(similar_train))
+        return left_train,left_dev,right_train,right_dev,similar_train,similar_dev
+    else:
+        return left_image_path_list,right_image_path_list,similar_list
+    
+def vectorize_imgs(img_path_list):
+    image_arr_list = []
+    for img_path in img_path_list:
+        
+        if os.path.exists(BASE_PATH + img_path):
+            img = Image.open(BASE_PATH + img_path)
+           
+            img_arr = np.asarray(img, dtype='float32')
+            
+        
+            image_arr_list.append(img_arr)
+        else:
+            print(img_path)
+    return image_arr_list
+
+
+def get_batch_image_path(left_train, right_train, similar_train, start):
+    end = (start + batch_size) % len(similar_train)
+    if start < end:
+        return left_train[start:end], right_train[start:end], similar_train[start:end], end,0
+    # 当 start > end 时，从头返回
+    return np.concatenate([left_train[start:], left_train[:end]]), \
+           np.concatenate([right_train[start:], right_train[:end]]), \
+           np.concatenate([similar_train[start:], similar_train[:end]]), \
+           end,1
+
+
+def get_batch_image_array(batch_left, batch_right, batch_similar):
+    return np.asarray(vectorize_imgs(batch_left), dtype='float32') / 255., \
+           np.asarray(vectorize_imgs(batch_right), dtype='float32') / 255., \
+           np.asarray(batch_similar)[:, np.newaxis]
+
+
+if __name__ == '__main__':
+    dataset_divide()
---
+++ train_less_memory.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from model import SIAMESE
+from dataset_less_memory import *
+import logging
+import os
+
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
+                        datefmt='%b %d %H:%M')
+    
+    with tf.name_scope("in"):
+        left = tf.placeholder(tf.float32, [None, 72, 72, 3], name='left')
+        right = tf.placeholder(tf.float32, [None, 72, 72, 3], name='right')
+    with tf.name_scope("similarity"):
+        label = tf.placeholder(tf.int32, [None, 1], name='label')  # 1 if same, 0 if different
+        label = tf.to_float(label)
+    
+    left_output = SIAMESE().siamesenet(left, reuse=False)
+    
+    right_output = SIAMESE().siamesenet(right, reuse=True)
+    
+    model1, model2, distance, loss = SIAMESE().contrastive_loss(left_output, right_output, label)
+    
+    global_step = tf.Variable(0, trainable=False)
+    
+    
+    
+    train_step = tf.train.AdamOptimizer(0.0000005).minimize(loss, global_step=global_step) #小数点后7个0
+    print("the model has been built")
+    
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=20)
+        saver.restore(sess, './checkpoint/model_70000.ckpt')                  #此处输入要续接的模型
+    
+        # setup tensorboard
+        tf.summary.scalar('step', global_step)
+        tf.summary.scalar('loss', loss)
+        for var in tf.trainable_variables():
+            tf.summary.histogram(var.op.name, var)
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter('train.log', sess.graph)
+        
+        pairs_base_path = './datas/pairs/'
+        for a,b,pairs_files in os.walk(pairs_base_path):
+            break
+        
+        pairs_txt_now = 0
+        pairs_file = os.path.join(pairs_base_path,str(pairs_files[pairs_txt_now]).strip())
+        pairs_txt_now += 1
+        print("pairs files' name is " +str(pairs_file))
+        left_train,left_dev,right_train,right_dev,similar_train,similar_dev = get_dataset(pairs_file,devset_need_or_not = True)
+        left_dev_arr, right_dev_arr, similar_dev_arr = get_batch_image_array(left_dev, right_dev, similar_dev)
+        
+        print('num of left train is '+str(len(left_train)))
+        print('num of right train is '+str(len(left_train)))
+        print('num of similar train is '+str(len(left_train)))
+        # train iter
+        idx = 0
+        
+        for i in range(FLAGS.train_iter):
+            '''此处file_next_or_not表示是否把一对正负样本txt里的样本对遍历完，如果是则返回 '1' '''
+            batch_left, batch_right, batch_similar, idx, file_next_or_not = get_batch_image_path(left_train, right_train, similar_train, idx)
+            batch_left_arr, batch_right_arr, batch_similar_arr = \
+                get_batch_image_array(batch_left, batch_right, batch_similar)
+            if file_next_or_not == 1:
+                pairs_file = os.path.join(pairs_base_path,str(pairs_files[pairs_txt_now]).strip())
+                print('\n\n------next batch is from ' + str(pairs_files[pairs_txt_now]) +  ' .------\n\n')
+                pairs_txt_now += 1
+                if pairs_txt_now == len(pairs_files):
+                    pairs_txt_now = 0
+
+                left_train,right_train,similar_train = get_dataset(pairs_file,devset_need_or_not = False)
+            
+    
+            steps, l, summary_str = sess.run([train_step, loss, merged],
+                                         feed_dict={left: batch_left_arr, right: batch_right_arr, label: batch_similar_arr})
+            print('after '+str(i) + ' times training,loss is ' + str(l))
+            writer.add_summary(summary_str, i)
+    
+            #测试正确率
+            if (i + 1) % FLAGS.validation_step == 0:
+                print('------validation here------')
+                num_of_true = 0
+                for t in range(0,abs(int(DEV_NUMBER / 50))):
+                    val_distance_district = sess.run([distance],
+                                            feed_dict={left: left_dev_arr[50*t:50*(t+1),:,:,:], right: right_dev_arr[50*t:50*(t+1),:,:,:]})
+                    index = 0
+                    
+                    for j in val_distance_district[0]:
+                        if j >= 0:
+                            j = 1
+                        else:
+                            j = 0
+                        
+                        if j ==similar_dev_arr[50*t:50*(t+1)][index] :
+                            num_of_true += 1
+                        index += 1
+                
+                print('测试集的正确率为：' + str(1.0*num_of_true/2000))
+                        
+                
+    
+            if i % FLAGS.step == 0 and i != 0:
+                saver.save(sess, "checkpoint/model_%d.ckpt" % i)
+
+if __name__ == '__main__' :
+    main()
