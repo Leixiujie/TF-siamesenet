4c7a06baf70802b30975f0c5a2495c996ff30b4f
Leixiujie <982463478@qq.com>
2019-03-27 00:21:18 +0800

    update

--- softmax_dataset_generation.py
+++ softmax_dataset_generation.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
 Created on Mon Jan 14 12:53:47 2019
 
@@ -8,9 +8,11 @@ Created on Mon Jan 14 12:53:47 2019
 from PIL import Image
 import os
 import xlrd
+import csv
 import numpy as np
 import cv2
 import csv
+import tf_enhancement
 
 
 def all_pics_processing(root , transformed_images,enhancement = True):
@@ -20,11 +22,11 @@ def all_pics_processing(root , transformed_images,enhancement = True):
     iiii = 0
     for pic_name in files:
         iiii += 1
-        if iiii % 100 == 0:
+        if iiii % 10.0 == 0:
             print('正在处理第'+str(iiii) + "张图片。")
         pathss=os.path.join(path,pic_name)
         image=Image.open(pathss)
-        image=image.resize((72,72))
+        image=image.resize((600,600))                           #不用tf处理的时候用
         img = np.asarray(image,dtype = 'float32')
         shape = img.shape
             
@@ -35,7 +37,7 @@ def all_pics_processing(root , transformed_images,enhancement = True):
         if (enhancement):     
             if len(shape) == 2:
                 image = cv2.imread(pathss)
-                img = cv2.cvtColor(cv2.resize(image,(72,72)),cv2.COLOR_BGR2RGB)
+                img = cv2.cvtColor(cv2.resize(image,(600,600)),cv2.COLOR_BGR2RGB)
                 img = np.asarray(img,np.uint8)
                 image = Image.fromarray(img)
                 
@@ -44,17 +46,18 @@ def all_pics_processing(root , transformed_images,enhancement = True):
             #生成原样本的图片    
             new_pic_name = '0_'+ str(pic_name).strip()
             output_file = os.path.join(transformed_images,new_pic_name)
-            image.save(output_file)
+            image_=image.resize((224,224)) 
+            image_.save(output_file)
             
             #生成水平翻转图片
             new_pic_name = '1_'+ str(pic_name).strip()
             output_file = os.path.join(transformed_images,new_pic_name)
-            image_fan = image.transpose(Image.FLIP_LEFT_RIGHT)
+            image_fan = (image.transpose(Image.FLIP_LEFT_RIGHT)).resize((224,224)) 
             image_fan.save(output_file)
             
             
             #生成噪点图片
-            image_with_noise = get_noise(image)
+            image_with_noise = (get_noise(image)).resize((224,224)) 
             new_pic_name = '2_'+str(pic_name).strip()
             output_file = os.path.join(transformed_images,new_pic_name)
             image_with_noise.save(output_file)
@@ -62,122 +65,113 @@ def all_pics_processing(root , transformed_images,enhancement = True):
             #生成带噪点的水平翻转图片
             new_pic_name = '3_'+ str(pic_name).strip()
             output_file = os.path.join(transformed_images,new_pic_name)
-            image_fan = get_noise(image.transpose(Image.FLIP_LEFT_RIGHT))
+            image_fan = (get_noise(image.transpose(Image.FLIP_LEFT_RIGHT))).resize((224,224)) 
             image_fan.save(output_file)
             
             
-            #随机生成10组0~60
-            for i in range(4,14):
+            #随机生成10组0~60带噪点的旋转图片
+            for i in range(4,6):
                 degree = np.random.randint(0,60)
                 new_pic_name = str(i) + '_' + str(pic_name).strip()
                 output_file = os.path.join(transformed_images,new_pic_name)
-                image_new = image.rotate(degree)
+                noise_level = np.random.randint(1,3)
+                image_new = (get_noise(image.rotate(degree),noise_level)).resize((224,224)) 
                 image_new.save(output_file)
                 
                 
-            #生成0~-60度的随机旋转图片    
-            for i in range(14,24):
-                degree = np.random.randint(300,359)
-                new_pic_name = str(i) + '_' + str(pic_name).strip()
-                output_file = os.path.join(transformed_images,new_pic_name)
-                image_new = image.rotate(degree)
-                image_new.save(output_file)
-            
-            #生成0~60度的带噪点随机旋转图片
-            for i in range(24,34):
-                degree = np.random.randint(0,60)
-                new_pic_name = str(i) + '_' + str(pic_name).strip()
-                output_file = os.path.join(transformed_images,new_pic_name)
-                noise_level = np.random.randint(1,4)
-                image_new = get_noise(image.rotate(degree),noise_level)
-                image_new.save(output_file)
-            
             #生成0~-60度带噪点的随机角度旋转图片
-            for i in range(34,44):
+            for i in range(6,8):
                 degree = np.random.randint(300,359)
                 new_pic_name = str(i) + '_' + str(pic_name).strip()
                 output_file = os.path.join(transformed_images,new_pic_name)
                 noise_level = np.random.randint(1,3)
-                image_new = get_noise(image.rotate(degree),noise_level)
+                image_new = (get_noise(image.rotate(degree),noise_level)).resize((224,224)) 
                 image_new.save(output_file)
-            
+            '''
             #生成10组不同范围的剪切图片    
-            for i in range(44,64):
-                pixel_need_to_cut_left = np.random.randint(0,20)
-                pixel_need_to_cut_right = np.random.randint(0,20)
-                pixel_need_to_cut_top = np.random.randint(0,20)
-                pixel_need_to_cut_bottom = np.random.randint(0,20)
+            for i in range(10,12):
+                pixel_need_to_cut_left = np.random.randint(0,0.3*600)
+                pixel_need_to_cut_right = np.random.randint(0,0.3*600)
+                pixel_need_to_cut_top = np.random.randint(0,0.3*600)
+                pixel_need_to_cut_bottom = np.random.randint(0,0.3*600)
                 degree = np.random.randint(-60,60)
-                image_new = (image.rotate(degree)).crop((pixel_need_to_cut_left,pixel_need_to_cut_top,72 - pixel_need_to_cut_right,72 - pixel_need_to_cut_bottom))
-                image_new = image_new.resize((72,72))
+                image_new = (image.rotate(degree)).crop((pixel_need_to_cut_left,pixel_need_to_cut_top,600 - pixel_need_to_cut_right,600 - pixel_need_to_cut_bottom))
+                image_new = image_new.resize((224,224))
                 new_pic_name = str(i) + '_' + str(pic_name).strip()
                 output_file = os.path.join(transformed_images,new_pic_name)
                 image_new.save(output_file)
-          
+            '''
             '''生成三组剪切一半图片,
                 cut_direction == 0则减去左半边，
                 为1则减去右边半边
                 为2则减去上半边，
                 为3则减去下半边
             '''
-            for i in range(64,70):
-                cut_direction = np.random.randint(4)
+            for i in range(8,11):
+                cut_direction = 0
                 if cut_direction == 0:
-                    pixel_need_to_cut_left = np.random.randint(20,40)
-                    pixel_need_to_cut_right = np.random.randint(0,5)
-                    pixel_need_to_cut_top = np.random.randint(1,10)
-                    pixel_need_to_cut_bottom = np.random.randint(1,10)
-                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,72 - pixel_need_to_cut_right,72 - pixel_need_to_cut_bottom))
-                    image_new = image_new.resize((72,72))
+                    pixel_need_to_cut_left = np.random.randint(int(35.0/72.0*600),int(50.0/72.0*600))
+                    pixel_need_to_cut_right = np.random.randint(0,90)
+                    pixel_need_to_cut_top = np.random.randint(1,90)
+                    pixel_need_to_cut_bottom = np.random.randint(1,90)
+                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,600 - pixel_need_to_cut_right,600 - pixel_need_to_cut_bottom))
+                    image_new = image_new.resize((224,224))
                     new_pic_name = str(i) + '_' + str(pic_name).strip()
                     output_file = os.path.join(transformed_images,new_pic_name)
                     image_new.save(output_file)
                 elif cut_direction == 1:
-                    pixel_need_to_cut_left = np.random.randint(0,5)
-                    pixel_need_to_cut_right = np.random.randint(20,40)
-                    pixel_need_to_cut_top = np.random.randint(1,10)
-                    pixel_need_to_cut_bottom = np.random.randint(1,10)
-                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,72 - pixel_need_to_cut_right,72 - pixel_need_to_cut_bottom))
-                    image_new = image_new.resize((72,72))
+                    pixel_need_to_cut_left = np.random.randint(0,90)
+                    pixel_need_to_cut_right = np.random.randint(int(35.0/72.0*600),int(50.0/72.0*600))
+                    pixel_need_to_cut_top = np.random.randint(1,90)
+                    pixel_need_to_cut_bottom = np.random.randint(1,90)
+                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,600 - pixel_need_to_cut_right,600 - pixel_need_to_cut_bottom))
+                    image_new = image_new.resize((224,224))
                     new_pic_name = str(i) + '_' + str(pic_name).strip()
                     output_file = os.path.join(transformed_images,new_pic_name)
                     image_new.save(output_file)
                 elif cut_direction == 2:
-                    pixel_need_to_cut_left = np.random.randint(0,10)
-                    pixel_need_to_cut_right = np.random.randint(1,10)
-                    pixel_need_to_cut_top = np.random.randint(20,40)
-                    pixel_need_to_cut_bottom = np.random.randint(1,5)
-                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,72 - pixel_need_to_cut_right,72 - pixel_need_to_cut_bottom))
-                    image_new = image_new.resize((72,72))
+                    pixel_need_to_cut_left = np.random.randint(0,90)
+                    pixel_need_to_cut_right = np.random.randint(1,90)
+                    pixel_need_to_cut_top = np.random.randint(int(20.0/72.0*600),int(35.0/72.0*600))
+                    pixel_need_to_cut_bottom = np.random.randint(1,90)
+                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,600 - pixel_need_to_cut_right,600 - pixel_need_to_cut_bottom))
+                    image_new = image_new.resize((224,224))
                     new_pic_name = str(i) + '_' + str(pic_name).strip()
                     output_file = os.path.join(transformed_images,new_pic_name)
                     image_new.save(output_file)
                 elif cut_direction == 3:
-                    pixel_need_to_cut_left = np.random.randint(0,10)
-                    pixel_need_to_cut_right = np.random.randint(1,10)
-                    pixel_need_to_cut_top = np.random.randint(1,5)
-                    pixel_need_to_cut_bottom = np.random.randint(20,40)
-                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,72 - pixel_need_to_cut_right,72 - pixel_need_to_cut_bottom))
-                    image_new = image_new.resize((72,72))
+                    pixel_need_to_cut_left = np.random.randint(0,90)
+                    pixel_need_to_cut_right = np.random.randint(1,90)
+                    pixel_need_to_cut_top = np.random.randint(1,90)
+                    pixel_need_to_cut_bottom = np.random.randint(int(20.0/72.0*600),int(35.0/72.0*600))
+                    image_new = image.crop((pixel_need_to_cut_left,pixel_need_to_cut_top,600 - pixel_need_to_cut_right,600 - pixel_need_to_cut_bottom))
+                    image_new = image_new.resize((224,224))
                     new_pic_name = str(i) + '_' + str(pic_name).strip()
                     output_file = os.path.join(transformed_images,new_pic_name)
                     image_new.save(output_file)
-                  
+                cut_direction += 1
+        
+        else:
+            if len(shape) == 2:
+                image = cv2.imread(pathss)
+                img = cv2.cvtColor(cv2.resize(image,(600,600)),cv2.COLOR_BGR2RGB)    #不用tf处理的时候用
+                img = np.asarray(img,dtype = np.uint8)
+                image = Image.fromarray(img)            
         
             output_file = os.path.join(transformed_images,pic_name)
+            image = image.resize((224,224))
             image.save(output_file)
-
 
 
 '''此函数是获得噪声的函数'''
 def get_noise(img,noise_level = 1):
-    noise_level = 1
+    noise_level = 10
     img = np.asarray(img,np.uint8)
     img.flags.writeable = True
     row_num = 0
     for row in img:
         for i in range(noise_level):
-            img[row_num,np.random.randint(0,72),:] = 255
+            img[row_num,np.random.randint(0,600),:] = 255
         row_num += 1
         
     img = Image.fromarray(img)          
@@ -185,6 +179,8 @@ def get_noise(img,noise_level = 1):
     return img                                  #注意：此处返回去的是Image类型的文件
                 
 def csv_enhancement():
+    '''
+    xlrd版本加强csv
     workbook = xlrd.open_workbook('train.csv')     #从按图片排序的csv中读取
     imginfo = workbook.sheet_by_index(0)
     paths = imginfo.col_values(0)
@@ -195,25 +191,41 @@ def csv_enhancement():
     writer = csv.writer(f1)
     tt = 0
     
+
     for path in paths:
-        path_num = 0
-        for i in range(70):
-            path_name = str(path_num).strip()+'_'+str(path).strip()
+        
+
+        for i in range(0,64):
+            path_name = str(i).strip()+'_'+str(path).strip()
             writer.writerow((path_name,idds[tt]))
-            path_num += 1
+        
+        
+                
         tt += 1
     
     
     f1.close()
     f2.close()
-
-            
-            
+    '''
+    with open('train.csv') as f:
+        reader = csv.reader(f)
+        f1 = open('softmax_dataset.csv','w',newline='')
+        writer = csv.writer(f1)
+        iii = 0
+        for row in reader:
+            if iii == 15697:
+                break
+            for i in range(0,64):
+                path_name = str(i).strip()+'_'+str(row[0]).strip()
+                writer.writerow((path_name,row[1]))
+            iii += 1
+        f1.close()
             
 if __name__ == '__main__':
     root = './train/'
-    transformed_images = './softmax_train/'
+    transformed_images = './softmax_train1/'
     csv_enhancement()
-    all_pics_processing(root,transformed_images)
-    
-    
+    #all_pics_processing(root,transformed_images)
+    #all_pics_processing('./train/','./train_with_3_channels/',enhancement=False)
+    #tf_enhancement.generate('./train_with_3_channels/','./softmax_train1/')
+    all_pics_processing('./test/','./new_test/',enhancement=False)    
---
+++ tf_enhancement.py
@@ -0,0 +1,126 @@
+﻿
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import os
+
+
+
+filename = tf.placeholder(tf.string, [], name='filename')
+image_file = tf.read_file(filename)
+# Decode the image as a JPEG file, this will turn it into a Tensor
+image_raw = tf.image.decode_jpeg(image_file)  # 图像解码成矩阵
+image = 255.0 * tf.image.convert_image_dtype(image_raw, tf.float32)
+pics = []
+image_ = tf.image.resize_images(image,(299,299),0)
+pics.append(image_)
+
+image1 = tf.image.resize_images(image,(299,299),1)
+pics.append(image1)
+
+image1=tf.image.flip_up_down(image) #将图像从上至下顺时针翻转180°
+pics.append(image1)
+
+
+#产生灰度图
+image_gray = tf.image.rgb_to_grayscale(image)
+image_gray = tf.image.grayscale_to_rgb(image_gray)
+pics.append(image_gray)
+
+#生成均值为0的图
+adjusted = tf.image.per_image_standardization(image_raw)
+adjusted_image = 255.0 * tf.image.convert_image_dtype(adjusted, tf.float32)
+pics.append(adjusted_image)
+
+#生成不同亮度的图
+delta = -0.3
+while(delta <= 0.6):
+    image2 = tf.image.adjust_brightness(image_raw,delta)
+    delta += 0.3
+    image2 = 255.0 * tf.image.convert_image_dtype(image2, tf.float32)
+    pics.append(image2)
+
+#生成不同剪切范围的图
+crop_size = 80
+while(crop_size < 200):
+    random_size = int((np.random.randint(0,10)*1.0/15) * crop_size)
+    croped = tf.image.resize_image_with_crop_or_pad(image_,crop_size,crop_size+random_size)
+    croped = tf.image.resize_images(croped,(299,299),0)
+    pics.append(croped)   
+    
+    croped = tf.image.resize_image_with_crop_or_pad(image_,crop_size + random_size,crop_size)
+    croped = tf.image.resize_images(croped,(299,299),0)
+    pics.append(croped)
+    
+    croped = tf.image.resize_image_with_crop_or_pad(image_,crop_size,crop_size)
+    croped = tf.image.resize_images(croped,(299,299),0)
+    pics.append(croped)
+    crop_size += 40
+
+#生成不同对比度的图
+contrast = 0.1
+while(contrast <= 2.5):
+    contrast_pic = tf.image.adjust_contrast(image_raw,contrast)
+    contrast_pic = 255.0 * tf.image.convert_image_dtype(contrast_pic, tf.float32)
+    pics.append(contrast_pic)
+    contrast += 0.5
+    
+#生成不同色相的图
+hue = -0.5
+while(hue <= 0.5):
+    hue_image = tf.image.adjust_hue(image_raw,hue)
+    hue_image = 255.0 * tf.image.convert_image_dtype(hue_image, tf.float32)
+    pics.append(hue_image)
+    hue += 0.3
+
+#生成不同饱和度的图
+saturation = -5
+while(saturation <= 5):
+    saturation_image = tf.image.adjust_saturation(image_raw,saturation)
+    saturation_image = 255.0 * tf.image.convert_image_dtype(saturation_image, tf.float32)
+    pics.append(saturation_image)
+    saturation += 3
+
+def generate(origin,destin_file):
+    with tf.Session() as sess:
+      for a,b,pic_all in os.walk(origin):
+          break
+      print(len(pic_all))
+      iii = 0
+      for pic_name in pic_all:
+          if iii % 10.0 == 0:
+              print('正在处理第'+str(iii)+'张tf转换图片')
+          iii += 1
+          pic = os.path.join(origin,pic_name)
+          transformed_pics = sess.run(pics,feed_dict={filename:pic}) 
+          
+          num = 24
+          
+          if not os.path.exists(destin_file):
+              os.makedirs(destin_file)
+              
+          for pic in transformed_pics:
+              picture = np.asarray(pic,dtype=np.uint8)
+              image__ = Image.fromarray(picture)
+              image__ = image__.resize((299,299))
+              #image__.show()
+              image__.save(destin_file+str(num)+'_'+(str(pic_name).split('.')[0])+'.jpg')
+              num += 1
+          
+          degree = -90
+          picture = np.asarray(transformed_pics[0],dtype=np.uint8)
+          image_ = Image.fromarray(picture)
+          #用原图所生成-90~90度旋转图片
+          while(degree < 90): 
+              image__ = image_.rotate(degree)
+              image__ = image__.resize((299,299))
+              image__.save(destin_file+str(num)+'_'+(str(pic_name).split('.')[0]).strip()+'.jpg')
+              degree += 30
+              num += 1
+          
+if __name__ == '__main__':
+    origin = './train_with_3_channels/'
+    destin_file = './softmax_train/'
+    
+    
+    generate(origin,destin_file)
